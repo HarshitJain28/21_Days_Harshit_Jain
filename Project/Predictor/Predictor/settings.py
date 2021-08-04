4f4836146556874e391e4c7a629b2a4456d0f294
HarshitJain28 <66867971+HarshitJain28@users.noreply.github.com>
2021-08-05 01:00:09 +0530

    Made some Changes

--- Project/Predictor/Predictor/settings.py
+++ Project/Predictor/Predictor/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'predict',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
--- Project/Predictor/predict/views.py
+++ Project/Predictor/predict/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
-from PIL import Image
 from .models import Img
 from .forms import ImageForm
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 # Create your views here.
 def get_cropped_image_if_2_eyes(image_path):
-    print(image_path)
     img = cv2.imread(image_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -17,7 +15,7 @@ def get_cropped_image_if_2_eyes(image_path):
         roi_color = img[y:y+h, x:x+w]
         return roi_color
 
-class_labels = ['Ronaldo','Ibrahimovic','Messi','Mbappe','Neymar']
+class_labels = ['Cristiano Ronaldo','Zlatan Ibrahimovic','Lionel Messi','Mbappe','Neymar']
 
 path = r'C:\Users\flyas\Desktop\HarshitJain\21 Days\21_Days_Harshit_Jain\Project\my_model'
 
@@ -38,8 +36,13 @@ def index(request):
         if form.is_valid():
             form.save()
             img_obj = form.instance
-            result = predicting('.'+str(img_obj.image.url))
-            return render(request, 'predict/index.html', {'form': form, 'img_obj': img_obj, 'result': result})
+            try:
+                result = 'You Look Like : ' + predicting('.'+str(img_obj.image.url))
+                status = 'Match'
+            except:
+                result = "Input Image Can not be processed please change the image"
+                status = "No Match"
+            return render(request, 'predict/index.html', {'form': form, 'img_obj': img_obj, 'result': result, 'status':status})
     else:
         form = ImageForm()
     return render(request, 'predict/index.html',{'form': form})
